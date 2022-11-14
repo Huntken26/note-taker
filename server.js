53f03684d80613e28ff7227ca639a7c759b32f93
huntken26 <huntken26@gmail.com>
2022-11-14 07:04:48 -0600

    Added custom middleware/cLog, fsUtils.js file

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-// Added express/path dependencies and setyp variables
+// Added express/path dependencies and setyp variables.
 
 const express = require("express");
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const { clog } = require('./middleware/clog');
 
 //Added middleware 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+// Import custom middleware, "cLog"
+app.use(clog);
