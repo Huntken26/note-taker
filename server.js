b8cf0f71ef1f7a4f1e76f1af9c5d6f0bbf21ef70
huntken26 <huntken26@gmail.com>
2022-11-14 07:57:03 -0600

    Added get/listen routes to server.js

--- server.js
+++ server.js
@@ -13,3 +13,22 @@ app.use(express.static('public'));
 
 // Import custom middleware, "cLog"
 app.use(clog);
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for feedback page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// Wildcard route to direct users to the home page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
